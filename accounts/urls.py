d4484e2cb752bab632425a8d977ae06860b52204
BEATRIZ RODRIGUES <be.rodrigues.braga@gmail.com>
2026-06-18 13:33:32 -0300

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('cadastrar/', views.cadastrar, name='cadastrar'),
-    path('ativar/<uidb64>/<token>/', views.ativar_conta, name='ativa'),
-]
